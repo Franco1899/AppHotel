b66fb75799f4834ca79c0660048376c1e9595484
Franco <francoarmando1911@gmail.com>
2024-04-05 08:40:51 -0300

    4/4/2023

--- src/main/java/com/francoarmando/AppHotel/AppHotelApplication.java
+++ src/main/java/com/francoarmando/AppHotel/AppHotelApplication.java
@@ -3,11 +3,45 @@ package com.francoarmando.AppHotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class AppHotelApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppHotelApplication.class, args);
+
+		Connection connection = null;
+		try {
+			// Paso 1: Cargar el driver JDBC
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Paso 2: Establecer la conexión con la base de datos
+			String url = "jdbc:mysql://localhost:3306/RegistroHotel";
+			String usuario = "root";
+			String contraseña = "root";
+			connection = DriverManager.getConnection(url, usuario, contraseña);
+
+			System.out.println("Conexión exitosa a la base de datos MySQL");
+
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			// Paso 3: Cerrar la conexión
+			try {
+				if (connection != null) {
+					connection.close();
+					System.out.println("Conexión cerrada correctamente");
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 }
+
