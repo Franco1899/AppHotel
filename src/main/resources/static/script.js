242a16052970c22215a2aad61086a1eba65690be
Franco <francoarmando1911@gmail.com>
2024-04-06 20:17:41 -0300

    6/4/2023

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// modal variables
+
 const modal = document.querySelector('[data-modal]');
 const modalCloseBtn = document.querySelector('[data-modal-close]');
 const modalCloseOverlay = document.querySelector('[data-modal-overlay]');
 
-// modal function
+
 const modalCloseFunc = function () { modal.classList.add('closed') }
 
-// modal eventListener
+
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
 modalCloseBtn.addEventListener('click', modalCloseFunc);
 
@@ -16,11 +16,11 @@ modalCloseBtn.addEventListener('click', modalCloseFunc);
 
 
 
-// notification toast variables
+
 const notificationToast = document.querySelector('[data-toast]');
 const toastCloseBtn = document.querySelector('[data-toast-close]');
 
-// notification toast eventListener
+
 toastCloseBtn.addEventListener('click', function () {
   notificationToast.classList.add('closed');
 });
@@ -57,7 +57,7 @@ for (let i = 0; i < mobileMenuOpenBtn.length; i++) {
 
 
 
-// accordion variables
+
 const accordionBtn = document.querySelectorAll('[data-accordion-btn]');
 const accordion = document.querySelectorAll('[data-accordion]');
 
